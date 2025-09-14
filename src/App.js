f75b86dc9e2ae1067264276c4310b41f86b28f1f
vinit-rao <virtualvinityt@gmail.com>
2025-09-14 00:12:12 -0400

    t11

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import logo from './logo.svg';
 import React, {Component} from 'react'
 import './App.css';
 import Greet from './components/Greet';
-import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message'
+import Counter from './components/Counter'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        {/* <Welcome /> */}
-        {/* <Greet name="Vinit"/>
-        <Greet name="John Doe"/> */}
-        {/* <Hello /> */}
-        {/* <Welcome name="Vinit"/> */}
-        <Message />
+          <Counter />
       </div>
     )
   }
